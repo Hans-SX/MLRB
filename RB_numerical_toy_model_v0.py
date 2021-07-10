367709b18109347b361c1cca3d84c2e59f744b52
Hans-SX <shihxian.yang@gmail.com>
2021-07-10 20:00:23 +0800

    Toy model and fit are both working correctly

--- RB_numerical_toy_model_v0.py
+++ RB_numerical_toy_model_v0.py
@@ -3,6 +3,7 @@
 Created on Thu Jun 24 16:39:15 2021
 
 @author: Hans
+
 """
 import numpy as np
 from numpy.linalg import inv
@@ -10,7 +11,7 @@ from scipy.stats import unitary_group
 import matplotlib.pyplot as plt
 import time
 
-def sequence_with_noise(m, Lmbda, seed=1):
+def sequence_with_noise_v1(m, Lmbda):
     oper = np.eye(2)
     inver_op = np.eye(2)
     for i in range(m):
@@ -19,8 +20,39 @@ def sequence_with_noise(m, Lmbda, seed=1):
         # add noise and random unitary each iteration
         oper = np.matmul(Lmbda, np.matmul(u_map, oper))
         # add "u_(i+1)^(-1)" to "u_(i)^(-1)..."
-        inver_op = np.matmul(inv(u_map), inver_op)
+        inver_op = np.matmul(inver_op, inv(u_map))
     return inver_op, oper
+
+def sequence_with_noise_v2(m, rho, Lmbda):
+    # Output: state after all operation, including inv(U) and noise.
+    inver_op = np.eye(2)
+    for i in range(m):
+        u_map = unitary_group.rvs(2)
+        # add "Lamda u_(i+1)" to "Lmbda u_(i)..."
+        # add noise and random unitary each iteration
+        rho = u_map @ rho @ np.conj(u_map).T
+        rho = Lmbda @ rho @ np.conj(Lmbda).T
+        # add "u_(i+1)^(-1)" to "u_(i)^(-1)..."
+        inver_op = np.matmul(inver_op, inv(u_map))
+    n_rho = inver_op @ rho @ np.conj(inver_op).T
+    n_rho = Lmbda @ n_rho @ np.conj(Lmbda).T
+    return n_rho
+
+def sequence_with_noise_v3(m, rho, E1, E2):
+    # Output: state after all operation, including inv(U) and noise.
+    inver_op = np.eye(2)
+    for i in range(m):
+        u_map = unitary_group.rvs(2)
+        # add "Lamda u_(i+1)" to "Lmbda u_(i)..."
+        # add noise and random unitary each iteration
+        rho = u_map @ rho @ np.conj(u_map).T
+        rho = E1 @ rho @ np.conj(E1).T + E2 @ rho @ np.conj(E2).T
+        # add "u_(i+1)^(-1)" to "u_(i)^(-1)..."
+        inver_op = np.matmul(inver_op, inv(u_map))
+    n_rho = inver_op @ rho @ np.conj(inver_op).T
+    n_rho = E1 @ n_rho @ np.conj(E1).T + E2 @ n_rho @ np.conj(E2).T
+    return n_rho
+
 
 seed = 1
 np.random.seed(seed)
@@ -29,12 +61,17 @@ ket_0 = np.array([1,0]).reshape(2,1)
 ket_1 = np.array([0,1]).reshape(2,1)
 rho = np.array([[1,0],[0,0]])
 proj_O = np.kron(ket_0, ket_0.T)        # np.kron(ket_0, ket_0.T) - np.kron(ket_1, ket_1.T)
-pd = 1.333
-# 0.01 ~ 0.05 current best; 0.01 no better than 0.1; 0.0001 mess # 0.1 close, but not start from ~1
-D = 2^1     # Eq.2.9 in RB_Clifford 1811.10040
+# pd = 0.2   # according to RB_Clifford should be ~ 1.333
+# for v1, 0.01 ~ 0.013 current best; 0.01 no better than 0.1; 0.0001 mess # 0.1 close, but not start from ~1
+# D = 2     # Eq.2.9 in RB_Clifford 1811.10040
 # Lmbda = (1-pd)*np.eye(2) + (pd/D)*(np.kron(ket_0, ket_0.T) - np.kron(ket_1, ket_1.T))
-Lmbda = (1-pd)*np.eye(2) + (pd/D)*(np.kron(ket_0, ket_1.T) + np.kron(ket_1, ket_0.T))
-M = 40
+# Lmbda = (1-pd)*np.eye(2) + (pd/D)*(np.kron(ket_0, ket_1.T) + np.kron(ket_1, ket_0.T))
+# Lmbda = (1/np.sqrt(2))*(np.kron(ket_0, ket_0.T) - np.kron(ket_1, ket_1.T))
+Lmbda = np.kron(ket_0, ket_0.T) - np.kron(ket_1, ket_1.T)
+# Lmbda = np.eye(2)/np.sqrt(2)
+# E1 = np.eye(2)/np.sqrt(2)
+# E2 = (1/np.sqrt(2))*(np.kron(ket_0, ket_0.T) - np.kron(ket_1, ket_1.T))
+M = 20
 Fm = np.zeros(M)
 sample_size = int(1e2)
 fm = np.zeros(sample_size)
@@ -43,19 +80,21 @@ for m in range(1, M+1):
     for i in range(sample_size):
         # inver_op = u_(m)^(-1) u_(m-1)^(-1)...
         # oper = Lamda u_(m) Lmbda u_(m-1)...
-        inver_op, oper = sequence_with_noise(m, Lmbda, seed)
+        # inver_op, oper = sequence_with_noise_v1(m, Lmbda)
+        Sm_rho = sequence_with_noise_v2(m, rho, Lmbda)
+        # Sm_rho = sequence_with_noise_v3(m, rho, E1, E2)
+        # inver_op, oper = sequence_with_noise_v1(m, Lmbda)
         # Sm here only consider the no-dagger part.
         # Sm = Lmbda inver_op Lmbda ... u_2 Lmbda u_1
-        Sm = np.matmul(Lmbda, np.matmul(inver_op, oper))
+        # Sm = np.matmul(Lmbda, np.matmul(inver_op, oper))
         # Sm_rho = Sm rho Sm_dagger
-        Sm_rho = np.matmul(Sm, np.matmul(rho, np.conj(Sm).T))
+        # Sm_rho = inver_op @ n_
         fm[i] = np.trace(np.matmul(proj_O, Sm_rho).real)
         
-    Fm[m-1] = np.average(fm)
-    # if m % 10 == 0:
-    #     np.savetxt("../data/Fm_" + str(M) + "_avg_" + str(sample_size) + "_seed_" + str(seed) + "_" + time_mark + ".txt", Fm)
-        
-plt.plot(range(1, M+1), Fm, 'bo')
+    Fm[m-1] = np.average(fm)        
+    # if m % 40 == 0:
+    #     np.savetxt("../data/Fm_" + str(M) + "_avg_" + str(sample_size) + "_CP_seed_" + str(seed) + "_" + time_mark + ".txt", Fm)
+plt.plot(range(1, M+1), Fm, 'o')
 
 
     
--- RB_numerical_toy_model_v1.py
+++ RB_numerical_toy_model_v1.py
@@ -7,40 +7,133 @@ Created on Mon Jun 28 14:20:36 2021
 import numpy as np
 from numpy.linalg import inv
 from scipy.stats import unitary_group
+import matplotlib.pyplot as plt
+import time
+from fit_RB import compare_Fm
 
-ket_0 = np.array([1,0]).reshape(2,1)
-ket_1 = np.array([0,1]).reshape(2,1)
-rho = np.array([[1,0],[0,0]])
-proj_O = np.kron(ket_0, ket_0.T)        # np.kron(ket_0, ket_0.T) - np.kron(ket_1, ket_1.T)
-m = 3
-lmbda = 0.2
-K = np.zeros((4,2,2))
-K[0] = np.sqrt(1-3*lmbda/4)*np.eye(2)
-K[1] = np.sqrt(lmbda/4)*(np.kron(ket_0, ket_1.T) + np.kron(ket_1, ket_0.T))
-K[2] = np.sqrt(lmbda/4)*(-1j*np.kron(ket_0, ket_1.T) + 1j*np.kron(ket_1, ket_0.T))
-K[3] = np.sqrt(lmbda/4)*(np.kron(ket_0, ket_0.T) - np.kron(ket_1, ket_1.T))
-noise_on = np.zeros((2,2))
-inver_op = np.eye(2)
+class noise_model():
+    def __init__(self, noise_mode, noise_para):
+        self.mode = noise_mode
+        self.para = noise_para
+    def apply_noise(self, state):
+        if self.mode == 'depolar':
+            model = self.depolarizing_noise(state)
+            # tmp_rho = self.depolarizing_noise_v2()
+        elif self.mode == 'p_flip':
+            model = self.phase_flip(state)
+        elif self.mode == 'amp_damp':
+            model = self.amplitude_damping(state)
+        # elif noise_mode == 'b_flip':
+            # tmp_rho = b_flip()
+        return model
+        
+    def depolarizing_noise(self, rho):
+        # noise channel should be the same as sum over Kraus.
+        # noise_para range from 0~1.5, depolarizing channel.
+        noisy_rho = (1-self.para)*rho + (self.para/2)*np.eye(2)
+        return noisy_rho
+    
+    def depolarizing_noise_v2(self, rho):
+        # noise channel should be the same as sum over Kraus.
+        lm = 0.1  # range from 0~1.5, depolarizing channel.
+        noisy_rho = 1j*np.zeros((2,2))
+        lmbda = lm
+        K = 1j*np.zeros((4,2,2)) + 1j*np.zeros((4,2,2))
+        K[0] = np.sqrt(1-3*lmbda/4)*np.eye(2)
+        K[1] = np.sqrt(lmbda/4)*(np.kron(ket_0, ket_1.T) + np.kron(ket_1, ket_0.T))
+        K[2] = np.sqrt(lmbda/4)*(-1j*np.kron(ket_0, ket_1.T) + 1j*np.kron(ket_1, ket_0.T))
+        K[3] = np.sqrt(lmbda/4)*(np.kron(ket_0, ket_0.T) - np.kron(ket_1, ket_1.T))
+        for i in range(4):
+            noisy_rho += K[i] @ rho @ np.conj(K[i]).T
+            # print(noisy_rho)
+        return noisy_rho
+    
+    def phase_flip(self, rho):
+        # noise channel should be the same as sum over Kraus.
+        noisy_rho = 1j*np.zeros((2,2))
+        ket_0 = np.array([1,0]).reshape(2,1)
+        ket_1 = np.array([0,1]).reshape(2,1)
+        K = np.zeros((2,2,2)) + 1j*np.zeros((2,2,2))
+        K[0] = np.eye(2)*np.sqrt(1-self.para)
+        K[1] = np.sqrt(self.para)*(np.kron(ket_0, ket_0.T) - np.kron(ket_1, ket_1.T))
+        # print(K[0])
+        # print(K[1])
+        for i in range(1,2):
+            noisy_rho += K[i] @ rho @ np.conj(K[i]).T
+        return noisy_rho
+    
+    def amplitude_damping(self, rho):
+        # noise channel should be the same as sum over Kraus.
+        noisy_rho = 1j*np.zeros((2,2))
+        K = np.zeros((2,2,2)) + 1j*np.zeros((2,2,2))
+        K[0] = np.array([[1,0],[0, np.sqrt(1-self.para)]])
+        K[1] = np.array([[0, np.sqrt(self.para)],[0,0]])
+        # print(K[0])
+        # print(K[1])
+        for i in range(1,2):
+            noisy_rho += K[i] @ rho @ np.conj(K[i]).T
+        return noisy_rho
 
-# Kraus operators have 4 terms to sum, CP map (unitary, CPTP) may not be hermitian,
-# can not multiply rho after sum over.
-# Calculate A rho A+ each iteration.
-for i in range(m):
-    cp_map = unitary_group.rvs(2)
-    # CP on the state
-    cp_on = cp_map @ rho @ np.conj(cp_map).T
-    # noise Lambda affect the state
-    for k in range(4):
-        noise_on += K[k] @ cp_on @ np.conj(K[k]).T
-    inver_cp = np.matmul(inv(cp_map), inver_op)
-    
-Sm = np.matmul(inver_op, noise_oper)
-Sm_rho = np.matmul(Sm, np.matmul(rho, np.conj(Sm).T))
-Fm = np.trace(np.matmul(proj_O, Sm_rho).real)
-# F = np.trace(np.matmul(np.conj(Sm_rho).T, np.matmul(proj_O, Sm_rho)))
+def sequence_with_noise(m, rho, noise_mode, noise_para):
+    tmp_rho = rho
+    inver_op = np.eye(2)
+    for i in range(m):
+        u_map = unitary_group.rvs(2)
+        tmp_rho = u_map @ tmp_rho @ np.conj(u_map).T
+        
+        noise_type = noise_model(noise_mode, noise_para)
+        tmp_rho = noise_type.apply_noise(tmp_rho)
+        
+        inver_op = inver_op @ inv(u_map)
+    return tmp_rho, inver_op, noise_type
 
-print('Fm = ', Fm)
-# print('F =', F)
+
+if __name__ == "__main__":
     
+    noise_mode = 'amp_damp'
+    noise_para = 0.999
     
+    seed = 1
+    np.random.seed(seed)
+    time_mark = time.strftime("%Y_%m_%d_%H_%M", time.localtime())
     
+    ket_0 = np.array([1,0]).reshape(2,1) + 1j*np.zeros((2,1))
+    ket_1 = np.array([0,1]).reshape(2,1) + 1j*np.zeros((2,1))
+    
+    rho = np.array([[1,0],[0,0]]) + 1j*np.zeros((2,2))
+    # rho = np.array([[1,1],[1,1]])/2 + 1j*np.zeros((2,2))
+    proj_O = np.kron(ket_0, ket_0.T)        # np.kron(ket_0, ket_0.T) - np.kron(ket_1, ket_1.T)
+
+    M = 100
+    sample_size = int(1e1)
+    fm = np.zeros(sample_size)
+    Fm = np.zeros(M)
+    print('Setup state, projector, and some parameters.')
+    
+    # tmp_rho = phase_flip(rho)
+    
+    # Kraus operators have 4 terms to sum, CP map (unitary, CPTP) may not be hermitian,
+    # can not multiply rho after sum over.
+    # Calculate A rho A+ each iteration.
+    for m in range(1, M+1):
+        for i in range(sample_size):
+            tmp_rho, inver_op, noise_type = sequence_with_noise(m, rho, noise_mode, noise_para)
+            tmp_rho = inver_op @ tmp_rho @ np.conj(inver_op).T
+            final_state = noise_type.apply_noise(tmp_rho)
+            fm[i] = np.trace(proj_O @ final_state).real
+            
+        Fm[m-1] = np.average(fm)
+        print("m = ", str(m), " finished.")
+        # if m % 20 == 0:
+        #     np.savetxt("../data/Fm_" + str(M) + "_avg_" + str(sample_size) + '_' + noise_mode +'_'+ str(noise_para )+ "_seed_" + str(seed) + "_" + time_mark + ".txt", Fm)
+            # np.savetxt("../data/Fm_" + str(M) + "_avg_" + str(sample_size) + "_Amp_damp_001_K1_seed_" + str(seed) + "_" + time_mark + ".txt", Fm)
+    
+    pic_path = "../data/Fm_" + str(M) + "_avg_" + str(sample_size) + '_' + noise_mode +'_'+ str(noise_para) + "_seed_" + str(seed) + "_" + time_mark + ".png"
+    compare_Fm(Fm, proj_O, rho, noise_mode, noise_para, pic_path)
+    # plt.plot(range(1, M+1), Fm, 'o')
+    # plt.title(noise_mode)
+    # plt.xlabel('Sequence length')
+    # plt.ylabel('Average sequence fidelity')
+        
+        
+        
